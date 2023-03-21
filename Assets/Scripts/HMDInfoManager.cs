23edffed67b5d9890d4ffb19817eebee39dd6e73
Alessia Borriello <alessia.borriello89@gmail.com>
2023-03-21 22:40:09 +0000

    VR Controllers Simulation
    Implemented VR Controllers Simulation system, partially working (hands and head moving correctly but not registering any input) to enable debugging without VIVE headset.

---
+++ Assets/Scripts/HMDInfoManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class HMDInfoManager : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log("Is Device Active " + XRSettings.isDeviceActive);
+        //Debug.Log("Device Name is : " + XRSettings.loadedDeviceName);
+
+        if (!XRSettings.isDeviceActive)
+        {
+            Debug.Log("No Headset pugged");
+        }
+        else if(XRSettings.isDeviceActive && (XRSettings.loadedDeviceName == "Mock HMD" || XRSettings.loadedDeviceName == "MockHMDDisplay"))
+        {
+            Debug.Log("Using Mock HMD");
+        }
+        else
+        {
+            Debug.Log("Headset plugged in is: " + XRSettings.loadedDeviceName);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
